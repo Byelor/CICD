c88365bcc9950614504ad0e4b90757088649d0df
Byelor <zaqw1234bmbmbm@gmail.com>
2026-02-25 12:11:48 +0300

    add define precomp

--- main.cpp
+++ main.cpp
@@ -7,9 +7,24 @@ int sumTwoInt(int a, int b)
 }
 
 
+#define TEST(condition, message) \
+    do { \
+        if (!condition) { \
+        cerr << "TEST ERROR << endl; \
+        cerr << "Test: " << __func__ << endl; \
+        cerr << "Message: " << message << endl; \
+        cerr << "File: " << __FILE__ << "Line: " << __LINE__ << endl; \
+        abort(); \
+    } \
+    while(0); \
+
+
 int main(){
 
 cout << "Hello world\n";
 
+cerr << "crap!!!";
+
 cout << sumTwoInt(10,20);
+
 }

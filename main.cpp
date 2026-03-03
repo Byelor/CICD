0cc99e8a05b83955fcc8d729ee48453cea1e96f4
Byelor <zaqw1234bmbmbm@gmail.com>
2026-03-03 10:38:40 +0300

    add tests

--- main.cpp
+++ main.cpp
@@ -1,8 +1,20 @@
 #include <iostream>
+#include "LinkedList.h"
 using namespace std;
 
 
 int main(int argc, char* argv[])
 {
-    cout << "hello world";
+    if(argc > 1)
+    {
+        if(argv[0] == "test")
+        {
+            LinkedList<int> list;
+            list.push_back(5);
+            list.push_back(2);
+            cout << list.top();
+            cout << list.pop();
+            cout << list.top();
+        }
+    }
 }
